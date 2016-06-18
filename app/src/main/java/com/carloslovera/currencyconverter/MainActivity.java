46cf253cf26111dfce562099f3c5c0fbcbf4a379
calovera <clovera@bu.edu>
2016-06-17 22:21:05 -0400

    Initial commit

---
+++ app/src/main/java/com/carloslovera/currencyconverter/MainActivity.java
@@ -0,0 +1,59 @@
+package com.carloslovera.currencyconverter;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void convert(View view) {
+        EditText dollars = (EditText)findViewById(R.id.dollarField);
+
+
+        Double dollarAmount = Double.parseDouble(dollars.getText().toString());
+        Double rate = 0.89;
+
+        Double euros = dollarAmount * rate;
+      //  Log.i("Info",euros.toString());
+        Toast.makeText(getApplicationContext(), "Euros: " + euros.toString() ,Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
